4adadf89372cacb6411fb01e488373b695a08883
Yilen <yilen.pan@gmail.com>
2016-01-02 11:36:15 -0800

    added jscrsc

--- app/commandsCTRL/updateCommands.js
+++ app/commandsCTRL/updateCommands.js
@@ -8,7 +8,7 @@ var coreUtils = require('../packages/core-utils');
 module.exports = function (packageObj, commandObj) {
   // Init commandObj
   commandObj = commandObj || {};
-  // turn current package's props to lower case 
+  // turn current package's props to lower case
   commandObj.packageCommands = lowerCaseProps(packageObj);
   commandObj.rawCommands = _.extend(coreUtils, packageObj);
   commandObj.parsedCommands = parseCommands(commandObj.rawCommands);
